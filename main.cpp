d0abe666988db074b24e1fd567518ce0675d05a5
mr90733 <mr90733@mail.harpercollege.edu>
2026-04-26 18:29:43 -0500

    TheSequel

--- main.cpp
+++ main.cpp
@@ -1,7 +1,73 @@
 #include <iostream>
 
-#include "src/hello.hpp"
+#include <string>
 
-int main() {
-  std::cout << hello() << std::endl;
+#include <iterator>
+
+using namespace std;
+
+int lengthOfString(const string& text)
+
+{
+    if (text.empty())
+
+    {
+        return 0;
+    }
+
+    return 1 + lengthOfString(text.substr(1));
+}
+
+void generateStrings(char collection[], int size, string& CString, int k)
+
+{
+    if (CString.length() == k)
+
+    {
+        cout << CString << endl;
+
+        return;
+    }
+
+    for (int i = 0; i < size; i++)
+
+    {
+        CString.push_back(collection[i]);
+
+        generateStrings(collection, size, CString, k);
+
+        CString.pop_back();
+    }
+}
+
+int main()
+
+{
+    string word;
+
+    cout << "Enter a string: ";
+
+    cin >> word;
+
+    cout << " Length: " << lengthOfString(word) << endl;
+
+    cout << endl;
+
+    char collection[] = {'a', 'b', 'c', 'd'};
+
+    int size = std::size(collection);
+
+    int k;
+
+    cout << "Enter k: ";
+
+    cin >> k;
+
+    cout << "Strings:" << endl;
+
+    string current;
+
+    generateStrings(collection, size, current, k);
+
+    return 0;
 }
